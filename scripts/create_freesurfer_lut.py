c1ff2c5eb8b002587f9acea0db9b938ca2775048
jorgenriseth <jnriseth@gmail.com>
2023-03-09 16:49:31 +0100

    created script for creating LUT

---
+++ scripts/create_freesurfer_lut.py
@@ -0,0 +1,38 @@
+import os
+import re
+from typing import Dict
+from pathlib import Path
+import json
+
+import pandas as pd
+import dataclasses
+
+
+@dataclasses.dataclass
+class LutEntry:
+    index: int
+    description: str
+
+
+def find_lut_entry(line):
+    m = re.match("^(\d+)\s+([a-zA-Z-]+)\s.", line)
+    if m is None:
+        return None
+    return LutEntry(index=int(m.group(1)), description=m.group(2))
+
+
+def to_dict(dataframe: pd.DataFrame) -> Dict[int, str]:
+    return dataframe.set_index("lut_index").to_dict()["description"]
+
+
+if __name__ == "__main__":
+    freesurfer_lut = Path(os.environ["FREESURFER_HOME"]) / "FreeSurferColorLUT.txt"
+    with open(freesurfer_lut, "r") as f:
+        lut_entries = list(filter(lambda x: x is not None, map(find_lut_entry, f)))
+        indices = pd.Series(map(lambda x: x.index, lut_entries), dtype=int)
+        descriptions = pd.Series(map(lambda x: x.description, lut_entries), dtype=str)
+
+    df = pd.DataFrame({"lut_index": indices, "description": descriptions})
+    df.set_index("lut_index")["description"].to_json(
+        "DATA/freesurfer_lut.json", orient="index", indent=4
+    )
